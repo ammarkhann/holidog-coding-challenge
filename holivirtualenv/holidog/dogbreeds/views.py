580d8ea7bb0d7805cc8e8e6998027a95cb71fbb9
ammarkhann <ammar88ammar@gmail.com>
2018-09-07 06:19:32 +0500

    Clean Code, Fixed issues with bootstrap

--- holivirtualenv/holidog/dogbreeds/views.py
+++ holivirtualenv/holidog/dogbreeds/views.py
@@ -8,33 +8,32 @@ import json
 def home(request):
 
 	response = json.loads(requests.get("https://dog.ceo/api/breeds/list/all").content)
-	print (response['message'])
 	dog_breed_list = []
 	for key,val in response['message'].items():
 		if response['message'][key]:
-			print (key,val , len(response['message'][key]))
-			for x in response['message'][key]:
-				dog_breed_list.append(key+"-"+x)
+			for nested_breed_types in response['message'][key]:
+				dog_breed_list.append(key+"-"+nested_breed_types)
 		else:
-			print (key,val)
 			dog_breed_list.append(key)
 
 	context = {
 		"dog_breed_list" : dog_breed_list,
 	}
 
-	return render(request,"home.html",context)
+	template = "home.html"
+
+	return render(request,template,context)
 
 
 
 def breed_image(request,slug):
 
-	print (slug)
-	response = json.loads(requests.get("https://dog.ceo/api/breed/"+slug+"/images/random").content)
-	
+	response = json.loads(requests.get("https://dog.ceo/api/breed/"+slug+"/images/random").content)	
 	context = {
 		"breed_image": response['message']
 
 	}
 
-	return render(request, "breed_image.html" ,context)
+	template = "breed_image.html"
+
+	return render(request,template ,context)
