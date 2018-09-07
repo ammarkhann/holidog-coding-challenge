36305d599b8e0497716b5b88e647a29ac5767f3c
ammarkhann <ammar88ammar@gmail.com>
2018-09-07 05:32:37 +0500

    Created First View To Show User A List Of Dog Breeds By Using The API

--- holivirtualenv/holidog/dogbreeds/views.py
+++ holivirtualenv/holidog/dogbreeds/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
-
+import requests
+import json
 # Create your views here.
+
+
+
+def home(request):
+
+	response = json.loads(requests.get("https://dog.ceo/api/breeds/list/all").content)
+	print (response['message'])
+	dog_breed_list = []
+	for key,val in response['message'].items():
+		if response['message'][key]:
+			print (key,val , len(response['message'][key]))
+			for x in response['message'][key]:
+				dog_breed_list.append(key+"-"+x)
+		else:
+			print (key,val)
+			dog_breed_list.append(key)
+
+	context = {
+		"dog_breed_list" : dog_breed_list,
+	}
+
+	return render(request,"home.html",context)
